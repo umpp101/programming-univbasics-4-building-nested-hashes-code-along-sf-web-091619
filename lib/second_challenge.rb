a553b3e120252892312caaf292687acc1550b190
Mustafa Muse <umpp101@icloud.com>
2019-09-07 19:10:16 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-      },
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
-      },
-      :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
-      },
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => [{
         :name => "Benvolio",
         :age => "17",
@@ -25,19 +15,9 @@ def second_challenge
       }] 
       }, 
     :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
-      },
-      :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
-      },
-      :heroine => {
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
-      },
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => [{
         :name => "Steven",
         :age => "30",
