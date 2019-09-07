3a8b1239f27e992002ef5ff5f3b9754ff2a5db7e
Mustafa Muse <umpp101@icloud.com>
2019-09-07 19:10:47 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,15 +4,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => [{
-        :name => "Benvolio",
-        :age => "17",
-        :attitude => "worried"
-      },{
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
-      }] 
+      :hero_friends => [] 
       }, 
     :capulet => {
       :patriarch => {},
